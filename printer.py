fd96a8adf3692dde3c06a5d145695caaf3b92419
Andrew Cole <aocole@gmail.com>
2013-02-17 15:49:03 -0800

    python is picky

--- printer.py
+++ printer.py
@@ -57,7 +57,7 @@ class Printer(threading.Thread):
 
 				# All done!
 				self.queue.task_done()
-				self.logger.debug("Finished queue item. Queue size: " + self.queue.qsize())
+				self.logger.debug("Finished queue item. Queue size: %i" % self.queue.qsize())
 			except Exception as e:
 				self.logger.error("Exception in printer: " + str(e))
 
