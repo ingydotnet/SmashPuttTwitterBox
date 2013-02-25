6f741f45e1116f3d4ed641a16afe003e011c35e8
Jacob Sayles <jacob@officenomads.com>
2013-02-24 16:37:38 -0800

    Removing lcd cruft

--- twitterbox.py
+++ twitterbox.py
@@ -42,12 +42,6 @@ def main():
 
 		# Setup the LCD display
 		GPIO.setmode(GPIO.BCM)	     # Use BCM GPIO numbers
-		GPIO.setup(LCD_E, GPIO.OUT)  # E
-		GPIO.setup(LCD_RS, GPIO.OUT) # RS
-		GPIO.setup(LCD_D4, GPIO.OUT) # DB4
-		GPIO.setup(LCD_D5, GPIO.OUT) # DB5
-		GPIO.setup(LCD_D6, GPIO.OUT) # DB6
-		GPIO.setup(LCD_D7, GPIO.OUT) # DB7
 
 		# Setup the alert light
 		GPIO.setup(LIGHT_PIN, GPIO.OUT) 
