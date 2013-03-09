19960f3c42e619701ff73f7e8e6268b17021dd89
Jacob Sayles <jacob@officenomads.com>
2013-03-09 12:38:19 -0800

    Handel sleeping when stream hits a twitter rate limit.

--- watcher.py
+++ watcher.py
@@ -1,7 +1,10 @@
 import threading
 import tweepy
-from settings import *
+import time
+
 from custom_stream_listener import CustomStreamListener
+import settings
+
 class Watcher(threading.Thread):
 	#----------------------------------------------------------------------
 	def __init__(self, queue, logger):
@@ -10,41 +13,47 @@ class Watcher(threading.Thread):
 		self.logger = logger
 		self.logger.debug("Twitter watcher created")
 		self.auth = None
-		self.api = None
 
 	#----------------------------------------------------------------------
 	def authenticate(self):
 		self.logger.info("Authenticating to twitter")
 		try:
-			self.auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-			self.auth.set_access_token(access_key, access_secret)
-			self.api = tweepy.API(self.auth)
+			self.auth = tweepy.OAuthHandler(settings.CONSUMER_KEY, settings.CONSUMER_SECRET)
+			self.auth.set_access_token(settings.ACCESS_KEY, settings.ACCESS_SECRET)
 		except Exception as e:
 			self.logger.error("Could not authenticate: " + str(e))
 
 	#----------------------------------------------------------------------
+	def get_api(self):
+		if not self.auth:
+			self.authenticate()
+		return tweepy.API(self.auth)
+
+	#----------------------------------------------------------------------
 	def run(self):
-		if not self.auth or not self.api:
+		if not self.auth:
 			self.authenticate()
 			
 		try:
 			listener = CustomStreamListener(self.queue, self.logger)
 			stream = tweepy.streaming.Stream(self.auth, listener, secure=True, headers={'User-Agent':'SmashPuttTwitterBox'})
 			self.logger.info("Starting twitter stream")
-			stream.filter(track=track)
+			stream.filter(track=settings.TRACK)
 			self.logger.error("Twitter stream closed")
+			if listener.rate_limited:
+				self.logger.debug("Sleeping for a minute to clear rate limit")
+				time.sleep(60)
 		except Exception as e:
 			self.logger.error("Disconnected from twitter: " + str(e))
 
 	#----------------------------------------------------------------------
 	def getUserData(self):
-		if not self.auth or not self.api:
-			self.authenticate()
+		api = get_api()
 
 		try:
-			user = self.api.get_user(screen_name)
-			user_data = {"@" + screen_name + ":": "Followers: " + str(user.followers_count)}
+			user = api.get_user(settings.SCREEN_NAME)
+			user_data = {"@" + settings.SCREEN_NAME + ":": "Followers: " + str(user.followers_count)}
 			return user_data
 		except Exception as e:
-			self.logger.error("Could not get data for " + screen_name + ": " + str(e))
+			self.logger.error("Could not get data for " + settings.SCREEN_NAME + ": " + str(e))
 
