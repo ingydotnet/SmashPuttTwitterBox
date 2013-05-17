fcbe79f286d00ac29b88468679cc78f389110586
Ingy döt Net <ingy@ingy.net>
2013-05-17 14:06:46 -0700

    More refactoring

--- setup.py
+++ setup.py
@@ -5,24 +5,35 @@ from distutils.core import setup
 setup(
     name='SmashPuttTwitterBox',
     version='0.0.1',
-    description='The uzh',
+    description="A Twitter Box for Smash Putt",
+    license='Python',
+    platforms=['POSIX'],
+    requires=[
+        'tweepy',
+        'pygame',
+        'RPi.GPIO',
+    ],
     author='Andrew Cole',
     author_email='aocole@aocole.net',
     url='https://github.com/aocole/SmashPuttTwitterBox',
-# License: UNKNOWN
-# Description: UNKNOWN
-# Platform: UNKNOWN
     packages=[
         'SmashPuttTwitterBox',
     ],
     scripts=[
         'scripts/smashputttwitterbox',
     ],
-#     package_dir={
-#         'SmashPuttTwitterBox': '.',
-#     },
     package_data={
         'SmashPuttTwitterBox': ['data/*'],
     },
+    classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Environment :: Web Environment',
+          'Intended Audience :: End Users/Desktop',
+          'License :: OSI Approved :: Python Software Foundation License',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Topic :: Communications :: Twitter',
+    ],
 
 )
